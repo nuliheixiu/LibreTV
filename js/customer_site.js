5e17fb9cc5fd983a6b6c16ef6c60d52f85290590
nuliheixiu <yulongzhao10001@gmail.com>
2025-08-17 19:03:39 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -1,17 +1,59 @@
 const CUSTOMER_SITES = {
-    qiqi: {
-        api: 'https://360zy.com/api.php/provide/vod',
-        name: 'haha资源',
-    },
     source1: {
         api: 'https://360zy.com/api.php/provide/vod',
         name: '360资源站',
     },
     source2: {
-        api: 'https://example.com/api.php/provide/vod',
-        name: '示例资源站',
+        api: 'https://cj.lziapi.com/api.php/provide/vod',
+        name: '量子',
+        adult: false
+    },
+    source3: {
+        api: 'http://ffzy.tv/api.php/provide/vod',
+        name: '非凡',
+        adult: false
+    },
+    source2: {
+        api: 'https://suoniapi.com/api.php/provide/vod/at/xml',
+        name: '索尼',
+        adult: false
+    },
+    source2: {
+        api: 'https://wolongzyw.com/api.php/provide/vod/?ac=list',
+        name: '卧龙',
+        adult: false
+    },
+    source2: {
+        api: 'https://www.ikunzy.com/api.php/provide/vod',
+        name: 'iKun',
+        adult: false
+    },
+    source2: {
+        api: 'https://api.ukuapi.com/api.php/provide/vod/?ac=list',
+        name: '尤酷',
+        adult: false
+    },
+    source2: {
+        api: 'https://jszyapi.com/api.php/provide/vod/?ac=list',
+        name: '极速',
+        adult: false
+    },
+    source2: {
+        api: 'https://suoniapi.com/api.php/provide/vod/?ac=list',
+        name: '率率',
+        adult: false
+    },
+    source2: {
+        api: 'https://api.apibdzy.com/api.php/provide/vod?ac=list',
+        name: '百度',
+        adult: false
+    },
+    source2: {
+        api: 'https://bfzyapi.com/api.php/provide/vod/?ac=list',
+        name: '暴风',
         adult: false
     }
+
 };
 
 // 调用全局方法合并
