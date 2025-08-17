4daa51362b3a9838c536b3038b77a7f088ad1284
nuliheixiu <yulongzhao10001@gmail.com>
2025-08-17 19:45:26 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -54,88 +54,88 @@ const CUSTOMER_SITES = {
         adult: false
     }.
     dyttzy: {
-       api: "http://caiji.dyttzyapi.com/api.php/provide/vod",
-       name: "电影天堂资源",
-       detail: "http://caiji.dyttzyapi.com"
+        api: "http://caiji.dyttzyapi.com/api.php/provide/vod",
+        name: "电影天堂资源",
+        detail: "http://caiji.dyttzyapi.com"
     },
     heimuer: {
-      api: "https://json.heimuer.xyz/api.php/provide/vod",
-      name: "黑木耳",
-      detail: "https://heimuer.tv"
+        api: "https://json.heimuer.xyz/api.php/provide/vod",
+        name: "黑木耳",
+        detail: "https://heimuer.tv"
     },
     ruyi: {
-      api: "http://cj.rycjapi.com/api.php/provide/vod",
-      name: "如意资源"
+        api: "http://cj.rycjapi.com/api.php/provide/vod",
+        name: "如意资源"
     },
     bfzy: {
-      api: "https://bfzyapi.com/api.php/provide/vod",
-      name: "暴风资源"
-    },
+        api: "https://bfzyapi.com/api.php/provide/vod",
+        name: "暴风资源"
+   },
     tyyszy: {
-      api: "https://tyyszy.com/api.php/provide/vod",
-      name: "天涯资源"
+        api: "https://tyyszy.com/api.php/provide/vod",
+        name: "天涯资源"
     },
     ffzy: {
-      api: "http://ffzy5.tv/api.php/provide/vod",
-      name: "非凡影视",
-      detail: "http://ffzy5.tv"
+       api: "http://ffzy5.tv/api.php/provide/vod",
+       name: "非凡影视",
+       detail: "http://ffzy5.tv"
     },
     zy360: {
-      api: "https://360zy.com/api.php/provide/vod",
-      name: "360资源"
+       api: "https://360zy.com/api.php/provide/vod",
+       name: "360资源"
     },
     maotaizy: {
-      api: "https://caiji.maotaizy.cc/api.php/provide/vod",
-      name: "茅台资源"
+       api: "https://caiji.maotaizy.cc/api.php/provide/vod",
+       name: "茅台资源"
     },
     wolong: {
-      api: "https://wolongzyw.com/api.php/provide/vod",
-      name: "卧龙资源"
+       api: "https://wolongzyw.com/api.php/provide/vod",
+       name: "卧龙资源"
     },
     jisu: {
-      api: "https://jszyapi.com/api.php/provide/vod",
-      name: "极速资源",
-      detail: "https://jszyapi.com"
+       api: "https://jszyapi.com/api.php/provide/vod",
+       name: "极速资源",
+       detail: "https://jszyapi.com"
     },
     dbzy: {
-      api: "https://dbzy.tv/api.php/provide/vod",
-      name: "豆瓣资源"
+       api: "https://dbzy.tv/api.php/provide/vod",
+       name: "豆瓣资源"
     },
     mozhua: {
-      api: "https://mozhuazy.com/api.php/provide/vod",
-      name: "魔爪资源"
+       api: "https://mozhuazy.com/api.php/provide/vod",
+       name: "魔爪资源"
     },
     mdzy: {
-      api: "https://www.mdzyapi.com/api.php/provide/vod",
-      "name": "魔都资源"
+       api: "https://www.mdzyapi.com/api.php/provide/vod",
+       name: "魔都资源"
     },
     zuid: {
-      api: "https://api.zuidapi.com/api.php/provide/vod",
-      name: "最大资源"
+       api: "https://api.zuidapi.com/api.php/provide/vod",
+       name: "最大资源"
     },
     yinghua: {
-      api: "https://m3u8.apiyhzy.com/api.php/provide/vod",
-      name: "樱花资源"
+       api: "https://m3u8.apiyhzy.com/api.php/provide/vod",
+       name: "樱花资源"
     },
     wujin: {
-      api: "https://api.wujinapi.me/api.php/provide/vod",
-      name: "无尽资源"
+       api: "https://api.wujinapi.me/api.php/provide/vod",
+       name: "无尽资源"
     },
     wwzy: {
-      api: "https://wwzy.tv/api.php/provide/vod",
-      name: "旺旺短剧"
+       api: "https://wwzy.tv/api.php/provide/vod",
+       name: "旺旺短剧"
     },
     ikun: {
-      api: "https://ikunzyapi.com/api.php/provide/vod",
-      name: "iKun资源"
+       api: "https://ikunzyapi.com/api.php/provide/vod",
+       name: "iKun资源"
     },
     lzi: {
-      api: "https://cj.lziapi.com/api.php/provide/vod",
-      name: "量子资源站"
+       api: "https://cj.lziapi.com/api.php/provide/vod",
+       name: "量子资源站"
     },
     xiaomaomi: {
-      api: "https://zy.xmm.hk/api.php/provide/vod",
-      name: "小猫咪资源"
+       api: "https://zy.xmm.hk/api.php/provide/vod",
+       name: "小猫咪资源"
     }
   }
 
