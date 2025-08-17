cfc047215d50e87d034c6d8735b6c49a08548318
nuliheixiu <yulongzhao10001@gmail.com>
2025-08-17 18:38:06 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -2,6 +2,16 @@ const CUSTOMER_SITES = {
     qiqi: {
         api: 'https://www.qiqidys.com/api.php/provide/vod',
         name: '七七资源',
+    },
+    source1: {
+        api: 'https://360zy.com/api.php/provide/vod',
+        name: '360资源站',
+        adult: false
+    },
+    source2: {
+        api: 'https://example.com/api.php/provide/vod',
+        name: '示例资源站',
+        adult: false
     }
 };
 
